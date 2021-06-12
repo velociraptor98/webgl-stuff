bde0a956acfe2663d0d8b4a04c8f5ed4392c8c7f
Kunal <kunal@smartserv.io>
2021-06-12 21:51:54 +0530

    Adding basic shaders

--- webgl.js
+++ webgl.js
@@ -1,3 +1,22 @@
+// Currently storing the shaders in same js file
+
+let vertexShaderText = [
+    'precision mediump float;',
+    'attribute vec2 vertPosition;',
+    'void main()',
+    '{',
+    'gl_Position = vec4(vertPosition,0.0,1.0);',
+    '}'
+].join('\n');
+
+let fragmentShaderText = [
+    'precision mediump float;',
+    'void main()',
+    '{',
+    'gl_FragColor = vec4(1.0,0.0,0.0,1.0);',
+    '}'
+].join('\n');
+
 
 let init = () => {
     let canvas = document.getElementById("webgl-surface");
@@ -12,5 +31,15 @@ let init = () => {
     }
     gl.clearColor(0.5,0.5,0.5,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    // Add a basic vertex and fragment shader
+    let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vertexShader,vertexShaderText);
+    gl.shaderSource(fragmentShader,fragmentShaderText);
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
 }
 init();
+
+
+
